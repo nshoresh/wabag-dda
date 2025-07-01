94e88a60e35f4388b44b1788fdec707c54fcd8ca
eugene-pande <esp.pandev@gmail.com>
2025-07-01 23:51:43 +1000

    Updated Administration and Development Plan Pages

--- resources/views/home/administration.blade.php
+++ resources/views/home/administration.blade.php
@@ -33,14 +33,6 @@
                         Home
                     </a>
                 </li>
-                <li>
-                    <div class="flex items-center">
-                        <svg class="w-5 h-5 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                            <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path>
-                        </svg>
-                        <a href="/about-wabag-dda" class="ml-1 text-gray-600 hover:text-wabag-blue transition-colors md:ml-2">About Wabag DDA</a>
-                    </div>
-                </li>
                 <li aria-current="page">
                     <div class="flex items-center">
                         <svg class="w-5 h-5 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
@@ -166,7 +158,7 @@
                                     <p class="text-gray-600 text-sm">Supporting the Provincial Administrator in all executive functions</p>
                                 </div>
                             </div>
-                        </div>
+                        </div> 
                     </div>
                 </div>
 
